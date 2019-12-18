be953b1737930fd8fa6520052da862524f204a99
aleksandrmanko <51031408+aleksandrmanko@users.noreply.github.com>
2019-12-18 22:47:45 +0300

    Initial commit

---
+++ Data_collecting.py
@@ -0,0 +1,12 @@
+import os
+import openpyxl
+
+#Program coolecting data from excel tables in one
+
+#Collecting data from tables in dictionary
+
+print('Enter the number of tables ')
+n = input() #Number of tables
+N = n + 1 #For circle
+
+for T in range(1,N):
